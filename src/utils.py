03cad1e074433ffbe5a6bc00fb7e95e6faf2e04b
David Anugraha <david.anugraha@gmail.com>
2024-03-12 23:08:38 -0400

    Added GAT model and CellLine generic model

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
 # For systems
 import os
 
+import torch
+
 CUR_FILE_PATH = os.path.abspath(__file__)
 SRC_PATH = os.path.dirname(CUR_FILE_PATH)
 
 RANDOM_SEED = 413
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
