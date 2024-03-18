b8f2b4d105252339d77ea375c49aab2df9268170
davidanugraha <52416943+davidanugraha@users.noreply.github.com>
2024-03-18 15:18:51 -0400

    Refined model structure, modify preprocessing so it's running and introduce target class features, provide general framework on training (#10)

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,26 @@
 # For systems
 import os
+import logging
 
+# For external libraries
 import torch
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Training related global variables
+RANDOM_SEED = 413
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+TARGET_CLASSES = ['target_class_kidney', 'target_class_lung', 'target_class_breast',
+                  'target_class_hematopoietic_lymphoid', 'target_class_colon', 'target_class_prostate',
+                  'target_class_ovary', 'target_class_skin', 'target_class_brain']
+
+# Path definitions
 CUR_FILE_PATH = os.path.abspath(__file__)
 SRC_PATH = os.path.dirname(CUR_FILE_PATH)
 
-RANDOM_SEED = 413
+# Define the dataset directory path
+DATASET_DIR = os.path.join(os.path.dirname(SRC_PATH), 'datasets')
+if not os.path.exists(DATASET_DIR):
+    os.makedirs(DATASET_DIR)
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
